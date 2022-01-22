95d28a227ed1e7109ba5fcfa85069128c0030b1b
Ssavan99 <58150774+Ssavan99@users.noreply.github.com>
2022-01-22 15:42:12 -0600

    added user and request model

---
+++ StudyMate/Models/Request.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyMate.Models
+{
+    public class Request
+    {
+    }
+}
---
+++ StudyMate/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyMate.Models
+{
+    public class User
+    {
+        public int nuId;
+        public string firstName;
+        public string lastName;
+        public string major;
+        public string emailId;
+        public List<string> intClass;
+        public List<string> skillSets;
+
+        public User()
+        {
+            this.NuId = nuId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Major = major;
+            this.EmailId = emailId;
+            this.IntClass = intClass;
+            this.SkillSets = skillSets;
+        }
+
+        public int NuId { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Major { get; set; }
+        public string EmailId { get; set; }
+        public List<string> IntClass { get; set; }
+        public List<string> SkillSets { get; set; }
+        
+    }
+}
