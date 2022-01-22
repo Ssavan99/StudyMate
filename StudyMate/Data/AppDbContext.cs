d5fa564b08f34adccaba3527406411823b265625
Sifat Syed <sifatsyed2000@gmail.com>
2022-01-22 15:20:56 -0600

    Added basic database info

--- StudyMate/Data/AppDbContext.cs
+++ StudyMate/Data/AppDbContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace StudyMate.Data
 {
-    public class AppDbContext
+    public class AppDbContext: DbContext
     {
+        public AppDbContext(DbContextOptions options) :
+           base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<Message> Message { get; set; }
     }
 }
